cdc9ddae145b6f996fdf81f305ab44554610ad21
Graham Campbell <graham@mineuk.com>
2013-12-08 18:28:25 +0000

    Fixes

--- src/assets/js/cms-cloudflare.js
+++ src/assets/js/cms-cloudflare.js
@@ -5,16 +5,18 @@ $(document).ready(function() {
         dataType: "html",
         timeout: 5000,
         success: function(data, status, xhr) {
-            $('#data').fadeOut(200, function() {
-                this.addClass('well');
-                this.html(data);
-                this.fadeIn(200);
+            var data = $('#data');
+            data.fadeOut(200, function() {
+                data.addClass('well');
+                data.html(data);
+                data.fadeIn(200);
             });
         },
         error: function(xhr, status, error) {
-            $('#data').fadeOut(200, function() {
-                this.html('<p class="lead">There was an error getting the data</p>');
-                this.fadeIn(200);
+            var data = $('#data');
+            data.fadeOut(200, function() {
+                data.html('<p class="lead">There was an error getting the data</p>');
+                data.fadeIn(200);
             });
         }
     });
