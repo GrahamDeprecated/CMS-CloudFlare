e604811b1da22d05bc1f8c21442c7604cc04ba81
Graham Campbell <graham@mineuk.com>
2013-12-08 17:58:21 +0000

    Missing semicolons

--- src/assets/js/cms-cloudflare.js
+++ src/assets/js/cms-cloudflare.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
         $('#data').fadeOut(200, function() {
             this.addClass('well');
             this.html(result);
-            this.fadeIn(200)
+            this.fadeIn(200);
         });
         
     });
     cmsCloudFlareRequest.error(function(jqXHR, textStatus, errorThrown) {
         $('#data').fadeOut(200, function() {
             this.html('<p class="lead">There was an error getting the data</p>');
-            this.fadeIn(200)
+            this.fadeIn(200);
         });
     });
 });
