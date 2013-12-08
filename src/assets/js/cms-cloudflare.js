bfc5b62be9fbbbba1a40a1cc1816cbfa3ba564cc
Graham Campbell <graham@mineuk.com>
2013-12-08 17:57:37 +0000

    Javascript transitions

--- src/assets/js/cms-cloudflare.js
+++ src/assets/js/cms-cloudflare.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
     var cmsCloudFlareRequest = $.get(cmsCloudFlareURL);
     cmsCloudFlareRequest.success(function(result) {
-        $('#data').addClass('well');
-        $('#data').html(result);
+        $('#data').fadeOut(200, function() {
+            this.addClass('well');
+            this.html(result);
+            this.fadeIn(200)
+        });
+        
     });
     cmsCloudFlareRequest.error(function(jqXHR, textStatus, errorThrown) {
-        $('#data').html('<p class="lead">There was an error getting the data</p>');
+        $('#data').fadeOut(200, function() {
+            this.html('<p class="lead">There was an error getting the data</p>');
+            this.fadeIn(200)
+        });
     });
 });
