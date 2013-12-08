7c998399bfad9e71d912742a481fca21d56f48ce
Graham Campbell <graham@mineuk.com>
2013-12-08 18:06:39 +0000

    Ajax tweaks

--- src/assets/js/cms-cloudflare.js
+++ src/assets/js/cms-cloudflare.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
-    var cmsCloudFlareRequest = $.get(cmsCloudFlareURL);
-    cmsCloudFlareRequest.success(function(result) {
-        $('#data').fadeOut(200, function() {
-            this.addClass('well');
-            this.html(result);
-            this.fadeIn(200);
-        });
-        
-    });
-    cmsCloudFlareRequest.error(function(jqXHR, textStatus, errorThrown) {
-        $('#data').fadeOut(200, function() {
-            this.html('<p class="lead">There was an error getting the data</p>');
-            this.fadeIn(200);
-        });
+    $.ajax({
+        url: cmsCloudFlareURL,
+        type: "GET",
+        dataType: "html",
+        timeout: 5000,
+        success: function(data, status, xhr) {
+            $('#data').fadeOut(200, function() {
+                this.addClass('well');
+                this.html(data);
+                this.fadeIn(200);
+            });
+        },
+        error: function(xhr, status, error) {
+            $('#data').fadeOut(200, function() {
+                this.html('<p class="lead">There was an error getting the data</p>');
+                this.fadeIn(200);
+            });
+        }
     });
 });
