3b9339d3c19e6d675e3854ed8efee6b83afd29b7
Graham Campbell <graham@mineuk.com>
2013-12-08 18:47:22 +0000

    Javascript derp

--- src/assets/js/cms-cloudflare.js
+++ src/assets/js/cms-cloudflare.js
@@ -5,18 +5,18 @@ $(document).ready(function() {
         dataType: "html",
         timeout: 5000,
         success: function(data, status, xhr) {
-            var data = $('#data');
-            data.fadeOut(200, function() {
-                data.addClass('well');
-                data.html(data);
-                data.fadeIn(200);
+            var area = $('#data');
+            area.fadeOut(200, function() {
+                area.addClass('well');
+                area.html(data);
+                area.fadeIn(200);
             });
         },
         error: function(xhr, status, error) {
-            var data = $('#data');
-            data.fadeOut(200, function() {
-                data.html('<p class="lead">There was an error getting the data</p>');
-                data.fadeIn(200);
+            var area = $('#data');
+            area.fadeOut(200, function() {
+                area.html('<p class="lead">There was an error getting the data</p>');
+                area.fadeIn(200);
             });
         }
     });
